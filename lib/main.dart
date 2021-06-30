57a0e674e985d3c7fab886421767600a5d196870
cigdem-kls <klscigdem03@gmail.com>
2021-06-30 13:21:37 +0300

    değişiklik yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:birdilimmutluluk/BackgroundImage.dart';
 import 'package:birdilimmutluluk/degerlendirme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   final pages = [
